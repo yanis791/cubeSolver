b80dd6462574a093ede812a46d3d69d3996cd189
yanis791 <734493541@qq.com>
2021-03-23 23:49:08 +0800

    raw code with bug

--- include/cubesolve.hpp
+++ include/cubesolve.hpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+string cube;
+
+
+
 typedef struct node
 {
   /* data */
@@ -13,7 +17,7 @@ typedef struct node
   struct node *R;
 
   int depth;
-  char type;
+  string type;
 }treeNode,*treeRoot;
 
 class cubesolve
@@ -23,41 +27,89 @@ private:
   string F,B,U,D,L,R;
   string operate;
 public:
-  cubesolve(string cubeOrigin);
+  cubesolve(string cube_origin);
   void turnF();
   void turnU();
   void turnR();
+  void turn(string type);
   void cubeSwitch(char &a,char &b,char &c,char &d);
   void show();
+  bool check();
+  bool checkEach(string str);
+  string showOperate();
+  void addOperate(string type);
+
 };
-
-cubesolve::cubesolve(string cubeOrigin)
-{
-  F.assign(cubeOrigin,0,4);
-  cubeOrigin.erase(0,4);
-
-  R.assign(cubeOrigin,0,4);
-  cubeOrigin.erase(0,4);
-
-  B.assign(cubeOrigin,0,4);
-  cubeOrigin.erase(0,4);
-
-  L.assign(cubeOrigin,0,4);
-  cubeOrigin.erase(0,4);
-
-  U.assign(cubeOrigin,0,4);
-  cubeOrigin.erase(0,4);
-
-  D.assign(cubeOrigin,0,4);
-  cubeOrigin.erase(0,4);
-}
-
-
-
-string cube;
-
 
 void treeCreate(treeRoot &root, int depth,char type);
 void treeDisplay(treeRoot root);
+cubesolve cubeSearch(treeRoot &root,cubesolve cube);
 
+void cubesolve::addOperate(string type){
+  operate.append(type);
+}
+
+
+string cubesolve::showOperate(){
+  return operate;
+}
+
+cubesolve::cubesolve(string cube_origin)
+{
+  F.assign(cube_origin,0,4);cube_origin.erase(0,4);
+  R.assign(cube_origin,0,4);cube_origin.erase(0,4);
+  B.assign(cube_origin,0,4);cube_origin.erase(0,4);
+  L.assign(cube_origin,0,4);cube_origin.erase(0,4);
+  U.assign(cube_origin,0,4);cube_origin.erase(0,4);
+  D.assign(cube_origin,0,4);cube_origin.erase(0,4);
+}
+
+void cubesolve::turn(string type){
+  if(type == "F") turnF();
+  if(type == "U") turnU();
+  if(type == "R") turnR();
+}
+
+void cubesolve::turnF(){
+  cubeSwitch(U[3],R[0],D[1],L[2]);
+  cubeSwitch(U[2],R[3],D[0],L[1]);
+  cubeSwitch(F[0],F[1],F[2],F[3]);
+}
+
+void cubesolve::turnR(){
+  cubeSwitch(U[2],B[0],D[2],F[2]);
+  cubeSwitch(U[1],B[2],D[1],F[1]);
+  cubeSwitch(R[0],R[1],R[2],R[3]);
+}
+
+void cubesolve::turnU(){
+  cubeSwitch(B[1],R[1],F[1],L[1]);
+  cubeSwitch(B[0],R[0],F[0],L[0]);
+  cubeSwitch(U[0],U[1],U[2],U[3]);
+}
+
+void cubesolve::show(){
+  cout<<F+R+B+L+U+D<<endl;
+}
+
+void cubesolve::cubeSwitch(char &a,char &b,char &c,char &d){
+    char e;
+    e = a;
+    a = d;
+    d = c;
+    c = b;
+    b = e;
+}
+
+bool cubesolve::checkEach(string str){
+  if(str[0] == str[1] == str[2] == str[3])
+    return true;
+  else return false;
+}
+bool cubesolve::check(){
+  if(checkEach(F)&&checkEach(B)&&checkEach(U)&&
+      checkEach(D)&&checkEach(L)&&checkEach(R))
+    return true;
+  else return false;
+}
 #endif //CUBESOLVE_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,6 @@
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "cubesolve");
-
-  //  /node_namespace/node_name
-  ros::NodeHandle nodeHandle("~");
-
-  if (!nodeHandle.param<string>("cube", cube,"N"))
-  ROS_WARN_STREAM("Did not load node_rate.");
 
   cubesolve cube("000011112222333344445555");
   cube.turnF();
@@ -19,8 +12,9 @@ int main(int argc, char **argv)
   treeRoot root;
   treeCreate(root,0,'N');
   //treeDisplay(root);
-  
-  return 0;
+  cubesolve finish_cude = cubeSearch(root,cube);
+  cout<<cube.showOperate()<< endl;
+  return 0; 
 }
 
 void treeCreate(treeRoot &root, int depth, char type){
@@ -44,33 +38,16 @@ void treeDisplay(treeRoot root){
   }
 }
 
-void cubesolve::turnF(){
-  cubeSwitch(U[3],R[0],D[1],L[2]);
-  cubeSwitch(U[2],R[3],D[0],L[1]);
-  cubeSwitch(F[0],F[1],F[2],F[3]);
-}
-
-void cubesolve::turnR(){
-  cubeSwitch(U[2],B[0],D[2],F[2]);
-  cubeSwitch(U[1],B[2],D[1],F[1]);
-  cubeSwitch(R[0],R[1],R[2],R[3]);
-}
-
-void cubesolve::turnU(){
-  cubeSwitch(B[1],R[1],F[1],L[1]);
-  cubeSwitch(B[0],R[0],F[0],L[0]);
-  cubeSwitch(U[0],U[1],U[2],U[3]);
-}
-
-void cubesolve::show(){
-  cout<<F+R+B+L+U+D<<endl;
-}
-
-void cubesolve::cubeSwitch(char &a,char &b,char &c,char &d){
-    char e;
-    e = a;
-    a = d;
-    d = c;
-    c = b;
-    b = e;
+cubesolve cubeSearch(treeRoot &root,cubesolve cube){
+  if(root){
+    if(root -> type != "N"){
+      cube.turn(root -> type);
+      cube.addOperate(root -> type);
+    }
+      
+    if(cube.check())  return cube;
+    cubeSearch(root -> F, cube);
+    cubeSearch(root -> U, cube);
+    cubeSearch(root -> R, cube);
+  }
 }
